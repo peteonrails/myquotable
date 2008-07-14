8865618382fd3209383ddade6603f703deb97fdf
Peter Jackson <pete@peteonrails.com>
2008-07-14 02:15:17 -0400

    Fixed user signup bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ActiveRecord::Base
   
   # prevents a user from submitting a crafted form that bypasses activation
   # anything else you want your user to change should be added here.
-  attr_accessible :login, :email, :password, :password_confirmation
+  attr_accessible :login, :email, :password, :password_confirmation, :first_name, :last_name
 
   acts_as_state_machine :initial => :pending
   state :passive
