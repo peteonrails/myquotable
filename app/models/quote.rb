51aed59961fae768ebd5c01f8d17711ad60e8180
Peter Jackson <pete@peteonrails.com>
2008-07-21 15:18:57 -0400

    Fixes mass assignment bug with author

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -9,6 +9,6 @@ class Quote < ActiveRecord::Base
   named_scope :recent, lambda { |*args| {:conditions => ["created_at > ?", (args.first || 2.weeks.ago).to_s(:db)]} }
   named_scope :descending, :order => "created_at DESC"
   
-  attr_accessible :quote, :quote_context, :quote_author, :quoted_at, :quoted_by, :private, :author_title, :copyright 
+  attr_accessible :quote, :quote_context, :author, :quoted_at, :quoted_by, :private, :author_title, :copyright 
 
 end
