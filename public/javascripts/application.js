0fa22491fd995efb28a4d4da7d042d69f33809a2
Peter Jackson <pete@peteonrails.com>
2008-08-19 15:28:54 -0400

    Remove "details" link from standard view and make the whole quote DIV clickable for the toggle effect.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,9 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+jQuery(document).ready(function() {
+	$("#quotes_container .quote").bind('click', function(e) {
+		if (e.target.tagName == "DIV") {
+			$(this).find(".quote_extended").toggle('blind');
+		}
+	});
+});
