e22cbdce69b8e26ab1fb5cbcbc2ce81fc0b88a75
Peter Jackson <pete@peteonrails.com>
2009-01-22 16:13:20 -0500

    Facebook placeholder

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -15,4 +15,7 @@ class ApplicationController < ActionController::Base
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
   # filter_parameter_logging :password
+
+  ensure_application_is_installed_by_facebook_user    
+
 end
