bc99dbe509ec2d7a74aea57d42e7e49b753d62d0
Peter Jackson <pete@peteonrails.com>
2009-01-23 09:42:47 -0500

    More facebook progress

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,5 +1,9 @@
 class InvitationsController < ApplicationController
+  def new 
+    @from_user_id = facebook_session.user.to_s  
+  end
+  
   def create 
-    @sent_to_id = params[:ids]  
+    @sent_to_ids = params[:ids]  
   end
 end
