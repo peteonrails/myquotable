d13091174f4d0b59b95044a26fde757d972b3ffb
Peter Jackson <pete@peteonrails.com>
2009-04-03 14:26:41 -0400

    Revert "More facebooky stuff."
    This reverts commit 1f8118c124d4b5355858d3f37faac166ed676408.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -14,7 +14,9 @@ class ApplicationController < ActionController::Base
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
   # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password   
+  # filter_parameter_logging :password
+
+  ensure_application_is_installed_by_facebook_user    
 
 <<<<<<< HEAD:app/controllers/application.rb
 <<<<<<< HEAD:app/controllers/application.rb
