a85f51dd66c307ce0ad272f2dab09be135d3956e
Peter Jackson <pete@peteonrails.com>
2009-04-03 14:25:01 -0400

    Revert "More facebook progress"
    This reverts commit bc99dbe509ec2d7a74aea57d42e7e49b753d62d0.
    
    Conflicts:
    
    	app/views/invitations/new.haml

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,9 +1,5 @@
 class InvitationsController < ApplicationController
-  def new 
-    @from_user_id = facebook_session.user.to_s  
-  end
-  
   def create 
-    @sent_to_ids = params[:ids]  
+    @sent_to_id = params[:ids]  
   end
 end
