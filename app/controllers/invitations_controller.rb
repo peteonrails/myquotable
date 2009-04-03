5ed2432972b7d78bf3a14796902752f8d64aacc6
Peter Jackson <pete@peteonrails.com>
2009-04-03 14:23:22 -0400

    Revert "Refactor the views. Add facebook id to user model."
    This reverts commit b086492e17d6cf855060f7463acb3c6e5a80e877.

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,21 +1,13 @@
 class InvitationsController < ApplicationController
-  layout :facebook
-  
   def new 
-    update_profile if should_update_profile?
+    if params[:from] 
+      @user = facebook_session.user 
+      @user.profile_fbml = render_to_string(:partial => "profile", :locals => {:from => params[:from], :user => @user})
+    end 
     @from_user_id = facebook_session.user.to_s  
   end
   
   def create 
     @sent_to_ids = params[:ids]  
-  end
-  
-  def should_update_profile?
-    params[:from]
-  end
-  
-  def update_profile
-    @user = facebook_session.user 
-    @user.profile_fbml = render_to_string(:partial => "profile", :locals => {:from => params[:from], :user => @user})
   end
 end
